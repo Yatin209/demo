a86c2f8a81907706eff7e8148d8e2818ed7e62c1
Yatin209 <thakkaryating@gmail.com>
2026-02-09 09:27:47 +0530

    Add files via upload

---
+++ Untitled-1.md
@@ -0,0 +1,2 @@
+// while doing this all your vpn should be off
+// your docker desktop should be open while performing this all tasks
---
+++ one.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class one {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of array:");
+        int n = sc.nextInt();
+
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+
+        System.out.println();
+        System.out.println("enter number of elements to be sifted:");
+        int s = sc.nextInt();
+        s = s % n;
+
+        int sorted[] = new int[n];
+        for(int i = 0; i < n; i++) {
+            sorted[(i + s) % n] = a[i];
+        }
+        System.out.println("sifted array is:");
+        for(int i = 0; i < n; i++) {
+            System.out.print(sorted[i] + " ");
+        }
+    }
+}
